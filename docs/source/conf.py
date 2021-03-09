9855b233023f2b4d4df35e56f764f93eeebba355
Tyler Kennedy <tk@tkte.ch>
2021-03-09 01:00:21 -0500

    Bump version: 0.3.2 → 0.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Tyler Kennedy'
 author = 'Tyler Kennedy'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.2'
+release = '0.4.0'
 
 
 # -- General configuration ---------------------------------------------------
